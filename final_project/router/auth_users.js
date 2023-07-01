51cc6f3b1aa106d13a458d90d44785cc5dabaa01
emanuellvz <emanuellapasta94@gmail.com>
2023-07-01 16:57:43 -0300

    cambios

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,76 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
-
+  const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error al intentar loguear"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn:10000});
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("Usuario logueado con éxito!");
+    } else {
+      return res.status(208).json({message: "Error al intentar loguear, revisa usuario y contraseña"});
+    }
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`Reseña para el libro ${isbn} ha sido añadida.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }
+  });
+
+  // Delete a book review
+
+regd_users.delete("/auth/review/:isbn", (req, res)=>{
+    const isbn = req.params.isbn;
+    const user = req.session.authorization["username"];
+    delete books[isbn]["reviews"][user];
+    res.send("Reseña quitada con éxito")
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,92 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
+
+const connectToURL = (url)=>{
+    const req = axios.get(url);
+    console.log(req);
+    req.then(resp => {
+        console.log("Fulfilled")
+        console.log(resp.data);
+    })
+    .catch(err => {
+        console.log("Rejected for url "+url)
+        console.log(err.toString())
+    });
+}
+
+function connectConParametro(url,parametro){
+    connectToURL(url+parametro)
+}
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "Usuario registrado!. Ya puedes loguearte"});
+      } else {
+        return res.status(404).json({message: "El usuario ya existe!"});    
+      }
+    } 
+    return res.status(404).json({message: "No se logró crear el usuario."});
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+ const booksbyauthor = [];
+ let isbns = Object.keys(books);
+ isbns.forEach((isbn) => {
+   if(books[isbn]["author"] === req.params.author) {
+     booksbyauthor.push({"isbn":isbn,
+                         "author":books[isbn]["author"],
+                         "title":books[isbn]["title"],
+                         "reviews":books[isbn]["reviews"]});              
+   } 
+})
+res.send(JSON.stringify({booksbyauthor}, null, 4));  
+})
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbyauthor.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});              
+      } 
+   })
+   res.send(JSON.stringify({booksbyauthor}, null, 4));  
+   })
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ const isbn=req.params.isbn;
+ res.send(JSON.stringify(books[isbn].reviews))
 });
 
+
 module.exports.general = public_users;
+
+connectToURL = ("https://emanuellapas-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/");
+connectConParametro("https://emanuellapas-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/",1)
+connectConParametro("https://emanuellapas-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/","Unknown")
+connectConParametro("https://emanuellapas-5000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/","Things Fall Apart");
